737c2c4e47299cc95d83e17877b1439e8d0c6197
Matias Seguel <matias.seguel.estay@gmail.com>
2016-09-30 13:07:00 -0300

    indicators added from api bsif

--- js/custom.js
+++ js/custom.js
@@ -31,3 +31,66 @@ $(function($){
 		return false
 	});
 
+	/*
+	-------------------------------------------------------------------------
+	INDICATORS
+	-------------------------------------------------------------------------
+	*/
+
+	// UF
+	$.ajax({
+		url: 'http://api.sbif.cl/api-sbifv3/recursos_api/uf?apikey=a646fc5d8a6d4ef520bc861dd412f95325178af3&formato=json',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(response) {
+		var uf = response.UFs[0].Valor;
+		$('.indicator-uf').text(uf);
+	});
+
+	// UTM
+	$.ajax({
+		url: 'http://api.sbif.cl/api-sbifv3/recursos_api/utm?apikey=a646fc5d8a6d4ef520bc861dd412f95325178af3&formato=json',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(response) {
+		var utm = response.UTMs[0].Valor;
+		$('.indicator-utm').text(utm);
+	});
+
+	// IPC
+	$.ajax({
+		url: 'http://api.sbif.cl/api-sbifv3/recursos_api/ipc?apikey=a646fc5d8a6d4ef520bc861dd412f95325178af3&formato=json',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(response) {
+		var ipc = response.IPCs[0].Valor;
+		$('.indicator-ipc').text(ipc);
+	});
+
+	// EURO
+	$.ajax({
+		url: 'http://api.sbif.cl/api-sbifv3/recursos_api/euro?apikey=a646fc5d8a6d4ef520bc861dd412f95325178af3&formato=json',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(response) {
+		var euro = response.Euros[0].Valor;
+		$('.indicator-euro').text(euro);
+	});
+
+	// DOLAR
+	$.ajax({
+		url: 'http://api.sbif.cl/api-sbifv3/recursos_api/dolar?apikey=a646fc5d8a6d4ef520bc861dd412f95325178af3&formato=json',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(response) {
+		var dolar = response.Dolares[0].Valor;
+		$('.indicator-dolar').text(dolar);
+	});
+});
+
+
